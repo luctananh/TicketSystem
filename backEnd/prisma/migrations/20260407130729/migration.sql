d45bba4c1507c28ce2e3d3d90fcdde3a7fc66ca0
Lục Anh <120559330+luctananh@users.noreply.github.com>
2026-04-11 10:55:19 +0700

    cập nhật cấu hình và kết nói prisma

--- backEnd/prisma/migrations/20260407130729/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "title" TEXT NOT NULL DEFAULT 'No Title';
--- backEnd/src/index.js
+++ backEnd/src/index.js
@@ -1,6 +1,18 @@
-const express = require('express')
+import { prisma } from '../lib/prisma';
+import express from 'express'
 const app = express()
 const port = 3000
+
+app.use(express.json());
+
+app.get('/get', async (req, res) => {
+    try {
+        const getuse = await prisma.user.findMany();
+        res.status(200).json(getuse);
+    } catch (error) {
+        res.status(500).json('khong co du lieu');
+    }
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
